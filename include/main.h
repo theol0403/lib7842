19544347d2f2a64f8191a44051145965429ef774
theol0403@gmail.com <theol0403@gmail.com>
2019-09-26 16:40:04 -0700

    ➕ Include okapi

--- include/main.h
+++ include/main.h
@@ -39,7 +39,7 @@
 /**
  * You should add more #includes here
  */
-//#include "okapi/api.hpp"
+#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
 /**
@@ -52,7 +52,7 @@
  */
 // using namespace pros;
 // using namespace pros::literals;
-// using namespace okapi;
+using namespace okapi;
 
 /**
  * Prototypes for the competition control tasks are redefined here to ensure
