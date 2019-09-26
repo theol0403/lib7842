9085bbf4e77864098024a06c2d55b2403c578ea4
Theo lemay <theol0403@gmail.com>
2019-09-25 23:48:58 -0700

    🚀 Switch clang script

--- run_clang-format.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /r src\ %%f in (*.cpp) do clang-format -style=file -i %%f
-for /r include\lib7842\ %%f in (*.hpp) do clang-format -style=file -i %%f
-
-
---
+++ run_clang-format.sh
@@ -0,0 +1,14 @@
+# @echo off
+# for /r src\ %%f in (*.cpp) do clang-format -style=file -i %%f
+# for /r include\lib7842\ %%f in (*.hpp) do clang-format -style=file -i %%f
+
+# find include/lib7842/ -iname *.hpp | xargs clang-format-6.0 -i -style=file
+# find src/ -iname *.cpp | xargs clang-format-6.0 -i -style=file
+
+for i in $(find src/ -iname *.cpp); do clang-format-9 -i -style=file $i
+done
+
+for i in $(find include/lib7842/ -iname *.hpp); do clang-format-9 -i -style=file $i
+done
+
+
