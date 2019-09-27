678c40bbff221ea254ca61db13c1accd40fda181
theol0403@gmail.com <theol0403@gmail.com>
2019-09-26 19:23:53 -0700

    ✨ Finish pathPoint

--- include/lib7842/purePursuit/pathPoint.hpp
+++ include/lib7842/purePursuit/pathPoint.hpp
@@ -7,11 +7,16 @@ namespace lib7842 {
 
 class PathPoint : public QPoint {
 
-  using pathData_t = std::variant<double, QLength, QSpeed>;
+  using pathData_t = std::variant<std::monostate, double, QLength, QSpeed>;
 
   public:
   void setData(std::string iid, pathData_t idata);
   pathData_t getData(std::string iid);
+
+  double getCurvature();
+  QLength getDistance();
+  QSpeed getVelocity();
+  int getSegmentIndex();
 
   protected:
   std::map<std::string, pathData_t> pathData {};
--- src/lib7842/purePursuit/pathPoint.cpp
+++ src/lib7842/purePursuit/pathPoint.cpp
@@ -9,7 +9,34 @@ void PathPoint::setData(std::string iid, pathData_t idata) {
 PathPoint::pathData_t PathPoint::getData(std::string iid) {
   try {
     return pathData.at(iid);
-  } catch(const std::out_of_range& e) { std::cerr << e.what() << std::endl; }
+  } catch(const std::out_of_range& e) {
+    std::cerr << "PathPoint::getData: " << e.what() << std::endl;
+    return pathData_t();
+  }
+}
+
+double PathPoint::getCurvature() {
+  try {
+    return std::get<double>(getData("curvature"));
+  } catch(const std::bad_variant_access& e) { return 0; }
+}
+
+QLength PathPoint::getDistance() {
+  try {
+    return std::get<QLength>(getData("distance"));
+  } catch(const std::bad_variant_access& e) { return 0_in; }
+}
+
+QSpeed PathPoint::getVelocity() {
+  try {
+    return std::get<QSpeed>(getData("velocity"));
+  } catch(const std::bad_variant_access& e) { return 0_mps; }
+}
+
+int PathPoint::getSegmentIndex() {
+  try {
+    return std::get<double>(getData("segmentIndex"));
+  } catch(const std::bad_variant_access& e) { return 0; }
 }
 
 } // namespace lib7842
