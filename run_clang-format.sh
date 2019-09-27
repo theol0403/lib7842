5a2d2f97b5b5bf8d1d0f4184ea7381cb2412b935
theol0403@gmail.com <theol0403@gmail.com>
2019-09-26 18:26:59 -0700

    🔧 Fix clang error

--- run_clang-format.sh
+++ run_clang-format.sh
@@ -5,10 +5,10 @@
 # find include/lib7842/ -iname *.hpp | xargs clang-format-6.0 -i -style=file
 # find src/ -iname *.cpp | xargs clang-format-6.0 -i -style=file
 
-for i in $(find src/ -iname *.cpp); do clang-format-9 -i -style=file $i
+for i in $(find src/ -iname *.cpp); do clang-format-9 -i -style=file $i || true
 done
 
-for i in $(find include/lib7842/ -iname *.hpp); do clang-format-9 -i -style=file $i
+for i in $(find include/lib7842/ -iname *.hpp); do clang-format-9 -i -style=file $i || true
 done
 
 
