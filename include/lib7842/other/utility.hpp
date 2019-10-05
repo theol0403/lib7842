5177ba12864be57fba32b0e8c249d0501b0812ae
Theo Lemay <theol0403@gmail.com>
2019-10-04 22:08:12 -0700

    🏗 Add QCurvature unit

--- include/lib7842/other/utility.hpp
+++ include/lib7842/other/utility.hpp
@@ -3,6 +3,25 @@
 
 namespace lib7842 {
 
+/**
+ * Define unitless unit
+ */
 constexpr Number number(1.0);
 
+/**
+ * Define Curvature dimention which is length-1
+ */
+QUANTITY_TYPE(0, -1, 0, 0, QCurvature)
+
+/**
+ * Curvature unit and literals
+ */
+constexpr QCurvature curvature(1.0);
+constexpr QCurvature operator"" _curv(long double x) {
+  return QCurvature(x);
 }
+constexpr QCurvature operator"" _curv(unsigned long long int x) {
+  return QCurvature(static_cast<double>(x));
+}
+
+} // namespace lib7842
