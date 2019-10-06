097e67496e7a632dc5d0c127c3de06680df21d8f
Theo Lemay <theol0403@gmail.com>
2019-10-05 23:40:43 -0700

    🚧 Start implementing CompoundPath - slicing problems [skip ci]

---
+++ src/lib7842/path/compoundPath.cpp
@@ -0,0 +1,50 @@
+#include "compoundPath.hpp"
+
+namespace lib7842 {
+
+CompoundPath& CompoundPath::addPoint(const QPoint& ipoint) {
+  path.push_back(ipoint);
+  return *this;
+}
+
+CompoundPath& CompoundPath::addSegment(const AbstractPath& isegment) {
+  path.push_back(isegment);
+  return *this;
+}
+
+CompoundPath& CompoundPath::addPoints(const std::vector<QPoint>& ipoints) {
+  for (auto&& ipoint : ipoints) {
+    addPoint(ipoint);
+  }
+  return *this;
+}
+
+CompoundPath& CompoundPath::addSegments(const std::vector<AbstractPath>& isegments) {
+  for (auto&& isegment : isegments) {
+    addSegment(isegment);
+  }
+  return *this;
+}
+
+SimplePath CompoundPath::extract() const {
+  ReferencePath temp = extractRef();
+  return SimplePath({temp.get().begin(), temp.get().end()});
+}
+
+ReferencePath CompoundPath::extractRef() const {
+  std::vector<ReferencePath::PointRef> temp;
+  for (auto&& segment : path) {
+    if (std::holds_alternative<QPoint>(segment)) {
+      temp.push_back(std::get<QPoint>(segment));
+    } else {
+      auto&& points = std::get<AbstractPath>(segment).extractRef();
+      for (auto&& point : points) {
+        temp.push_back(point);
+      }
+    }
+  }
+
+  return temp;
+}
+
+} // namespace lib7842
