d2557d6f7529bd7d1533d35c457b0a51b7eeb774
theol0403 <theol0403@gmail.com>
2019-10-05 22:16:31 -0700

    🚧 Implement ReferencePath

---
+++ include/lib7842/path/referencePath.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "main.h"
+#include "abstractPath.hpp"
+
+namespace lib7842 {
+
+class ReferencePath : public AbstractPath {
+
+ public:
+  /**
+   * Default Constructors
+   */
+  ReferencePath() = default;
+  ReferencePath(const ReferencePath& ipath) = default;
+  virtual ~ReferencePath() = default;
+
+  using PointRef = std::reference_wrapper<const QPoint>;
+
+  /**
+   * Explicit Constructors
+   */
+  ReferencePath(const std::vector<PointRef>& ipath);
+
+  /**
+   * Extractors
+   */
+  virtual SimplePath extract() const override;
+  virtual ReferencePath extractRef() const override;
+
+  std::vector<PointRef>& get();
+
+ protected:
+  std::vector<PointRef> path {};
+};
+
+} // namespace lib7842
