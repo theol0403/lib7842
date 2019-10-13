96782b397f68df0d9f532d1f20dd8dfea5a955c2
theol0403 <theol0403@gmail.com>
2019-10-12 18:17:25 -0700

    👷 Implement setDistancesTest

--- test/src/purePursuit/pathPackerTest.cpp
+++ test/src/purePursuit/pathPackerTest.cpp
@@ -1,5 +1,6 @@
 #include "test.hpp"
 #include "lib7842/purePursuit/pathPacker.hpp"
+#include "lib7842/purePursuit/linearGenerator.hpp"
 
 class PathPackerTest : public ::testing::Test {
 protected:
@@ -38,4 +39,22 @@ TEST_F(PathPackerTest, ComputeCurvatures) {
 
   ASSERT_EQ(pathTurn()[0].getData<QCurvature>("curvature"), 0_curv);
   ASSERT_EQ(pathTurn()[2].getData<QCurvature>("curvature"), 0_curv);
+}
+
+TEST_F(PathPackerTest, SetDistancesSimple) {
+  PackedPath path(SimplePath({{0_in, 0_in}, {0_in, 5_in}, {0_in, 10_in}}));
+  PathPacker::setDistances(path);
+
+  ASSERT_EQ(path()[0].getData<QLength>("distance"), 0_in);
+  ASSERT_EQ(path()[1].getData<QLength>("distance"), 5_in);
+  ASSERT_EQ(path()[2].getData<QLength>("distance"), 10_in);
+}
+
+TEST_F(PathPackerTest, SetDistancesComplex) {
+  PackedPath path(LinearGenerator::insert(SimplePath({{0_in, 0_in}, {0_in, 20_in}}), 1_in));
+  PathPacker::setDistances(path);
+
+  for (size_t i = 0; i < path().size(); i++) {
+    ASSERT_NEAR(path()[i].getData<QLength>("distance").convert(inch), i, 1e-8);
+  }
 }
