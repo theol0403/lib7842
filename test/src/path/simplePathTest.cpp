7b6a5d1348dfa26bc3c6f61b6c26bce028d9e670
theol0403 <theol0403@gmail.com>
2019-10-14 13:58:30 -0700

    👷 Reimplement Tests

--- test/src/path/simplePathTest.cpp
+++ test/src/path/simplePathTest.cpp
@@ -6,40 +6,39 @@ protected:
   Vector point1 {5_in, 3_in};
 };
 
-TEST_F(SimplePathTest, Constructors) {
+TEST_F(SimplePathTest, SimpleTest) {
   SimplePath path({point1});
   SimplePath path2 = path.extractCopy();
 }
 
-// TEST_F(SimplePathTest, Constructors) {
-//   SimplePath();
-//   SimplePath({point1});
-//   SimplePath({point1, point1});
-//   SimplePath(std::vector<Vector>({point1, point1}));
-// }
+TEST_F(SimplePathTest, Constructors) {
+  SimplePath();
+  SimplePath({point1});
+  SimplePath({point1, point1});
+}
 
-// TEST_F(SimplePathTest, ExtractReference) {
-//   SimplePath path({point1, point1});
+TEST_F(SimplePathTest, ExtractReference) {
+  SimplePath path({point1, point1});
 
-//   ReferencePath ipath = path.extractRef();
-//   ASSERT_EQ(ipath.get().size(), 2);
-//   for (auto&& point : ipath.get()) {
-//     ASSERT_EQ(point.get(), point1);
-//   }
+  ReferencePath ipath = path.extractRef();
+  ASSERT_EQ(ipath().size(), 2);
+  for (auto&& point : ipath()) {
+    ASSERT_EQ(point.get(), point1);
+  }
 
-//   ReferencePath ipath2 = path.extractRef();
-//   for (size_t i = 0; i < ipath2.get().size(); i++) {
-//     ASSERT_EQ(&ipath.get()[i].get(), &ipath2.get()[i].get());
-//     ASSERT_EQ(&ipath.get()[i].get(), &path.get()[i]);
-//   }
-// }
+  ReferencePath ipath2 = path.extractRef();
+  for (size_t i = 0; i < ipath2().size(); i++) {
+    ASSERT_EQ(&ipath()[i].get(), &ipath2()[i].get());
+    ASSERT_EQ(&ipath()[i].get(), path()[i].get());
+  }
+}
 
-// TEST_F(SimplePathTest, ExtractPath) {
-//   SimplePath path({point1, point1, point1});
+TEST_F(SimplePathTest, ExtractPath) {
+  SimplePath path({point1, point1, point1});
 
-//   SimplePath ipath = path.extract();
-//   ASSERT_EQ(ipath.get().size(), 3);
-//   for (auto&& point : ipath.get()) {
-//     ASSERT_EQ(point, point1);
-//   }
-// }
+  SimplePath ipath = path.extract();
+  ASSERT_EQ(ipath().size(), 3);
+  for (auto&& point : ipath()) {
+    ASSERT_EQ(*point, point1);
+  }
+}
