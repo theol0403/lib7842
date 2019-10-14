c6cc74a391f7a686886e8d59ea88d696697c9744
theol0403 <theol0403@gmail.com>
2019-10-13 21:56:29 -0700

    🎨 Add inches per second unit

--- include/lib7842/other/utility.hpp
+++ include/lib7842/other/utility.hpp
@@ -3,13 +3,21 @@
 
 namespace lib7842 {
 
+/////////////////
+//    Units    //
+/////////////////
+
 /**
- * Define unitless unit
+ * Unitless unit
  */
 constexpr Number number(1.0);
 
+///////////////////////
+// Units - Curvature //
+///////////////////////
+
 /**
- * Define Curvature dimension which is length-1
+ * Curvature dimension which is length-1
  */
 QUANTITY_TYPE(0, -1, 0, 0, QCurvature)
 
@@ -26,6 +34,32 @@ constexpr QCurvature operator"" _curv(long double x) {
 }
 constexpr QCurvature operator"" _curv(unsigned long long int x) {
   return QCurvature(static_cast<double>(x));
+}
+
+//////////////////////
+// Units - Velocity //
+//////////////////////
+
+/**
+ * Inches per second
+ */
+constexpr QSpeed ips = inch / second;
+constexpr QSpeed operator"" _ips(long double x) {
+  return static_cast<double>(x) * ips;
+}
+constexpr QSpeed operator"" _ips(unsigned long long int x) {
+  return static_cast<double>(x) * ips;
+}
+
+/**
+ * Inches per second squared
+ */
+constexpr QAcceleration ips2 = inch / (second * second);
+constexpr QAcceleration operator"" _ips2(long double x) {
+  return static_cast<double>(x) * ips2;
+}
+constexpr QAcceleration operator"" _ips2(unsigned long long int x) {
+  return static_cast<double>(x) * ips2;
 }
 
 } // namespace lib7842
