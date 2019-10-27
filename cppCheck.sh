23d9ae2f7084707a516bb45d844600e327ec1d97
theol0403 <theol0403@gmail.com>
2019-10-27 12:02:35 -0700

    🔧 Silence cppcheck

--- cppCheck.sh
+++ cppCheck.sh
@@ -3,10 +3,6 @@ for I in $(find include/lib7842/ -type d); do
     DIR=$DIR" -I$I"
 done
 
-for I in $(find test/include/ -type d); do
-    DIR=$DIR" -I$I"
-done
-
 for I in $(find include/okapi/ -type d); do
     DIR=$DIR" -I$I"
 done
