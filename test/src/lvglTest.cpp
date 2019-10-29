4ac43f9edb9f2b7a13829f72381bf9d7583cac27
theol0403 <theol0403@gmail.com>
2019-10-29 12:28:53 -0700

    ✨ Test OdomDebug

--- test/src/lvglTest.cpp
+++ test/src/lvglTest.cpp
@@ -1,10 +1,14 @@
 #include "test.hpp"
-#include "lvgl/lvgl.h"
 #include "main.h"
+
+#include "lib7842/gui/odomDebug.hpp"
 
 void lvglTest() {
 
-  lv_obj_t* button = lv_btn_create(lv_scr_act(), NULL);
+  // lv_obj_t* button = lv_btn_create(lv_scr_act(), NULL);
+
+  OdomDebug odom(lv_scr_act(), LV_COLOR_BLUE);
+  odom.initialize();
 
   while (true) {
     pros::delay(20);
