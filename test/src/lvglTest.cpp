c7d086f1d57d011d7c427026886360db5e87cc8d
Theo Lemay <theol0403@gmail.com>
2019-10-29 21:42:03 -0700

    📈 [LVGL] OdomDebug Test

--- test/src/lvglTest.cpp
+++ test/src/lvglTest.cpp
@@ -2,15 +2,53 @@
 #include "main.h"
 
 #include "lib7842/gui/odomDebug.hpp"
+#include "lib7842/odometry/customOdometry.hpp"
+
+class MockThreeEncXDriveModel : public ThreeEncXDriveModel {
+public:
+  MockThreeEncXDriveModel() :
+    ThreeEncXDriveModel(
+      std::make_shared<MockMotor>(),
+      std::make_shared<MockMotor>(),
+      std::make_shared<MockMotor>(),
+      std::make_shared<MockMotor>(),
+      std::make_shared<MockContinuousRotarySensor>(),
+      std::make_shared<MockContinuousRotarySensor>(),
+      std::make_shared<MockContinuousRotarySensor>()) {}
+
+  std::valarray<std::int32_t> getSensorVals() const override {
+    return std::valarray<std::int32_t> {leftEnc, rightEnc};
+  }
+
+  void setSensorVals(std::int32_t left, std::int32_t right, std::int32_t middle) {
+    leftEnc = left;
+    rightEnc = right;
+    middleEnc = middle;
+  }
+
+  std::int32_t leftEnc {0};
+  std::int32_t rightEnc {0};
+  std::int32_t middleEnc {0};
+};
 
 void lvglTest() {
 
   // lv_obj_t* button = lv_btn_create(lv_scr_act(), NULL);
 
-  OdomDebug odom(lv_scr_act(), LV_COLOR_BLUE);
-  odom.initialize();
+  auto model = std::make_shared<MockThreeEncXDriveModel>();
+  auto odom =
+    std::make_shared<CustomOdometry>(model, ChassisScales({{4_in, 10_in, 5_in, 4_in}, 360}));
+
+  OdomDebug debug(lv_scr_act(), LV_COLOR_HEX(0xFF7F00));
+  debug.initialize();
+
+  debug.attachOdom(odom);
+  // debug.attachResetter([&]() {
+  //   odom->reset();
+  // });
 
   while (true) {
-    pros::delay(20);
+    debug.render();
+    pros::delay(100);
   }
 }
