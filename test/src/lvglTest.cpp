47039a55c401e3e60b14b58a76d1b4bdb175792b
Theo Lemay <theol0403@gmail.com>
2019-10-29 21:48:02 -0700

    🎨 Clean main

--- test/src/lvglTest.cpp
+++ test/src/lvglTest.cpp
@@ -1,15 +1,9 @@
 #include "test.hpp"
 #include "main.h"
 
-#include "lib7842/gui/odomDebug.hpp"
-
 void lvglTest() {
 
-  // lv_obj_t* button = lv_btn_create(lv_scr_act(), NULL);
-
-
-  while (true) {
-    debug.render();
-    pros::delay(100);
-  }
+  // while (true) {
+  //   pros::delay(100);
+  // }
 }
