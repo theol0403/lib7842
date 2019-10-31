4a3132f0352ceef6d088f410fd4add3a4b2624c1
Theo Lemay <theol0403@gmail.com>
2019-10-31 07:24:41 -0700

    👷 [LVGL] Add LVGL screen tests

--- test/src/lvglTest.cpp
+++ test/src/lvglTest.cpp
@@ -1,9 +1,18 @@
 #include "test.hpp"
 #include "main.h"
 
+#include "lib7842/gui/screen.hpp"
+#include "lib7842/gui/odomDebug.hpp"
+
 void lvglTest() {
 
-  // while (true) {
-  // pros::delay(100);
-  // }
+  Screen scr(lv_scr_act(), LV_COLOR_ORANGE);
+
+  scr.makePage<OdomDebug>().attachOdom(nullptr).attachResetter(nullptr);
+
+  scr.startTask("Screen");
+
+  while (true) {
+    pros::delay(100);
+  }
 }
