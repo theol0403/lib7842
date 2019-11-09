23ce29f47ae91fb96cb53793a6d56b7d4b021721
Theo Lemay <theol0403@gmail.com>
2019-11-09 00:07:50 -0800

    🐛 Try to fix clangFormat.sh

--- clangFormat.sh
+++ clangFormat.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # @echo off
 # for /r src\ %%f in (*.cpp) do clang-format -style=file -i %%f
 # for /r include\lib7842\ %%f in (*.hpp) do clang-format -style=file -i %%f
@@ -5,12 +6,12 @@
 # find include/lib7842/ -iname *.hpp | xargs clang-format-6.0 -i -style=file
 # find src/ -iname *.cpp | xargs clang-format-6.0 -i -style=file
 
-for i in $(find src/ -iname *.cpp); do clang-format-9 -i -style=file $i || true
+for i in $(find ./src/ -iname "*.cpp"); do clang-format-9 -i -style=file $i || true
 done
 
-for i in $(find include/lib7842/ -iname *.hpp); do clang-format-9 -i -style=file $i || true
+for i in $(find ./include/lib7842/ -iname "*.hpp"); do clang-format-9 -i -style=file $i || true
 done
 
-for i in $(find test/src/ -iname *.cpp); do clang-format-9 -i -style=file $i || true
+for i in $(find ./test/src/ -iname "*.cpp"); do clang-format-9 -i -style=file $i || true
 done
 
