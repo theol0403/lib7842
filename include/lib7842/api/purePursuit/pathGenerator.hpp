8af8f4e9e439fcf7906391233d3b3645622c2048
Theo Lemay <theol0403@gmail.com>
2019-11-20 13:42:19 -0800

    Rework PathGenerator

--- include/lib7842/api/purePursuit/pathGenerator.hpp
+++ include/lib7842/api/purePursuit/pathGenerator.hpp
@@ -8,40 +8,59 @@ namespace lib7842 {
 
 class PathGenerator {
 public:
-  struct limits {
-    /**
-  	 * Minumum velocity for all points
-  	 */
-    const double min;
+  /**
+   * Generate a DataPath containing waypoint information for pure pursuit.
+   *
+   * @param  ipath      The path
+   * @param  minVel     The minimum velocity
+   * @param  maxVel     The maximum velocity
+   * @param  accel      The maximum acceleration
+   * @param  curvatureK How much to slow down around turns. This value is usually best around 0.5-2,
+   *                    0.5 tends to slow down around almost any curvature in the path, and 2 tends
+   *                    to slow down around only a very sharp curvature.
+   * @return the generated path
+   */
+  static DataPath
+    generate(const SimplePath& ipath, double minVel, double maxVel, double accel, double curvatureK);
 
-    /**
-  	 * Maximum velocity for all points
-  	 */
-    const double max;
-
-    /**
-  	 * Maximum Acceleration between points
-  	 */
-    const double accel;
-
-    /**
-     * How much to slow down around turns. This value is usually best around 0.5-2, 0.5 tends to
-     * slow down around almost any curvature in the path, and 2 tends to slow down around only a
-     * very sharp curvature.
-     */
-    const double curvatureK;
-
-    limits(QSpeed imin, QSpeed imax, QAcceleration iaccel, QCurvature icurvatureK);
-  };
-
-  static DataPath generate(const SimplePath& ipath, const limits& ilimits);
-
-  static void setDistances(DataPath& ipath);
+  /**
+   * Sets the waypoint curvatures.
+   *
+   * @param ipath The path
+   */
   static void setCurvatures(DataPath& ipath);
 
-  static void setMaxVelocity(DataPath& ipath, const limits& ilimits);
-  static void setMinVelocity(DataPath& ipath, const limits& ilimits);
+  /**
+   * Sets the waypoint velocities respecting curvature and deceleration. Traverses the path
+   * backwards using a rate limiter.
+   *
+   * @param ipath      The path
+   * @param maxVel     The maximum velocity
+   * @param accel      The accel
+   * @param curvatureK How much to slow down around turns. This value is usually best around 0.5-2,
+   *                   0.5 tends to slow down around almost any curvature in the path, and 2 tends
+   *                   to slow down around only a very sharp curvature.
+   */
+  static void setMaxVelocity(DataPath& ipath, double maxVel, double accel, double curvatureK);
 
+  /**
+   * Sets the waypoint minimum velocities respecting acceleration. Traverses the path forward using
+   * a rate limiter.
+   *
+   * @param ipath  The path
+   * @param minVel The minimum velocity
+   * @param accel  The accel
+   */
+  static void setMinVelocity(DataPath& ipath, double minVel, double accel);
+
+  /**
+   * Gets the curvature of a given segment.
+   *
+   * @param  prev  The previous point
+   * @param  point The current point
+   * @param  next  The next point
+   * @return The curvature
+   */
   static QCurvature getCurvature(const Vector& prev, const Vector& point, const Vector& next);
 };
 
