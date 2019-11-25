5716a5fce8c8a86bd887e74ef832dab3f60ba27d
Theo Lemay <theol0403@gmail.com>
2019-11-24 22:47:00 -0800

    Draft pursuitPath

---
+++ include/lib7842/api/purePursuit/pursuitPath.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "lib7842/api/positioning/path/discretePath.hpp"
+#include "pursuitLimits.hpp"
+#include <optional>
+
+namespace lib7842 {
+
+/**
+ * A DataPath that contains PursuitLimits.
+ */
+class PursuitPath : public DataPath {
+public:
+  /**
+   * Set the limits.
+   *
+   * @param ilimits The pursuit limits
+   */
+  void setLimits(const PursuitLimits& ilimits);
+
+  /**
+   * Get the limits. Will throw an exception if no limits are set.
+   *
+   * @return The limits.
+   */
+  PursuitLimits getLimits() const;
+
+  /**
+   * @return true if limits have been set.
+   */
+  bool hasLimits() const;
+
+protected:
+  std::optional<PursuitLimits> limits {std::nullopt};
+};
+} // namespace lib7842
