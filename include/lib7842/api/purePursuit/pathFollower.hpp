e0bd426bef3f4a61f2481d53fde55b23ce1037b3
Theo Lemay <theol0403@gmail.com>
2019-11-24 17:09:24 -0800

    Draft follower header

--- include/lib7842/api/purePursuit/pathFollower.hpp
+++ include/lib7842/api/purePursuit/pathFollower.hpp
@@ -3,16 +3,35 @@
 #include "lib7842/api/other/utility.hpp"
 #include "lib7842/api/positioning/path/discretePath.hpp"
 #include "okapi/api/chassis/model/chassisModel.hpp"
+#include "okapi/api/units/QLength.hpp"
+#include <optional>
 
 namespace lib7842 {
 
 class PathFollower {
 public:
   PathFollower(const std::shared_ptr<ChassisModel>& imodel,
-               const std::shared_ptr<Odometry>& iodometry);
+               const std::shared_ptr<Odometry>& iodometry,
+               const QLength& ilookahead);
+
+  void followPath(const DataPath& ipath);
 
 protected:
+  DataPath::array_t::iterator findClosest(const DataPath& ipath, const Vector& ipos);
+
+  Vector findLookaheadPoint(const Vector& ipos);
+
+  static std::optional<double> findIntersectT(const Vector& ifirst,
+                                              const Vector& isecond,
+                                              const Vector& ipos,
+                                              const QLength& ilookahead);
+
+  static QCurvature calculateCurvature(const State& istate, const Vector& ilookaheadPoint);
+
+  static std::valarray<double> calculateVelocity(double ivel, const QCurvature& icurvature);
+
   std::shared_ptr<ChassisModel> model {nullptr};
   std::shared_ptr<Odometry> odometry {nullptr};
+  QLength lookahead {0_in};
 };
 } // namespace lib7842
