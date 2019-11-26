da0fe1f84b91167ef4d1863f80b96491a1d0d274
Theo Lemay <theol0403@gmail.com>
2019-11-26 07:19:32 -0800

    Draft MathPoint

---
+++ include/lib7842/api/positioning/point/mathPoint.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "vector.hpp"
+
+namespace lib7842 {
+
+/**
+ * A 2D Point which is used for math operations
+ */
+class MathPoint {
+public:
+  double x {0}; // in meters
+  double y {0}; // in meters
+
+  MathPoint() = default;
+  MathPoint(const MathPoint& ipoint) = default;
+  virtual ~MathPoint() = default;
+
+  /**
+   * Create a new point
+   *
+   * @param ix The x
+   * @param iy The y
+   */
+  MathPoint(double ix, double iy);
+
+  /**
+   * Convert a Point to a MathPoint
+   *
+   * @param ipoint The point
+   */
+  explicit MathPoint(const Vector& ipoint);
+
+  /**
+   * Convert a MathPoint to a Vector
+   */
+  operator Vector() const;
+
+  /**
+   * Binary operators
+   */
+  MathPoint operator+(const MathPoint& rhs) const;
+  MathPoint operator-(const MathPoint& rhs) const;
+  bool operator==(const MathPoint& rhs) const;
+  bool operator!=(const MathPoint& rhs) const;
+
+  /**
+   * Scale operators
+   */
+  MathPoint operator*(const double scalar) const;
+  MathPoint operator/(const double scalar) const;
+
+  /**
+   * Utility functions
+   */
+  static double dist(const MathPoint& lhs, const MathPoint& rhs);
+
+  /**
+   * MathPoint operations
+   */
+  static MathPoint normalize(const MathPoint& point);
+  static double dot(const MathPoint& lhs, const MathPoint& rhs);
+  static double mag(const MathPoint& point);
+};
+
+} // namespace lib7842
