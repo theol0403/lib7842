6d4d4b5acc79413e93a51a86693261677b940b8b
Theo Lemay <theol0403@gmail.com>
2019-11-26 07:54:48 -0800

    Add sgn

--- include/lib7842/api/other/utility.hpp
+++ include/lib7842/api/other/utility.hpp
@@ -9,6 +9,13 @@ namespace lib7842 {
 using namespace okapi;
 
 /////////////////
+//    Sign    //
+/////////////////
+template <typename T> int sgn(T val) {
+  return (T(0) < val) - (val < T(0));
+}
+
+/////////////////
 //    Units    //
 /////////////////
 
