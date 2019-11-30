092268fbe7065d0ed70002edeaf21383583dcbe9
Theo Lemay <theol0403@gmail.com>
2019-11-29 22:29:58 -0800

    Add units file

---
+++ include/lib7842/api/other/units.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "okapi/api/units/QAcceleration.hpp"
+#include "okapi/api/units/QSpeed.hpp"
+#include "okapi/api/units/RQuantity.hpp"
+
+namespace lib7842::units {
+
+using namespace okapi;
+
+/**
+ * Inches per second
+ */
+constexpr QSpeed ips = inch / second;
+constexpr QSpeed operator"" _ips(long double x) {
+  return static_cast<double>(x) * ips;
+}
+constexpr QSpeed operator"" _ips(unsigned long long int x) {
+  return static_cast<double>(x) * ips;
+}
+
+/**
+ * Inches per second squared
+ */
+constexpr QAcceleration ips2 = inch / (second * second);
+constexpr QAcceleration operator"" _ips2(long double x) {
+  return static_cast<double>(x) * ips2;
+}
+constexpr QAcceleration operator"" _ips2(unsigned long long int x) {
+  return static_cast<double>(x) * ips2;
+}
+
+/**
+ * Units that represent a tile (2ft) and a court(12ft)
+ * Literals are `_tile` and `_court`, respectivly
+ */
+constexpr QLength tile = 2 * foot;
+constexpr QLength court = 12 * foot;
+constexpr QLength operator"" _tile(long double x) {
+  return static_cast<double>(x) * tile;
+}
+constexpr QLength operator"" _court(long double x) {
+  return static_cast<double>(x) * court;
+}
+constexpr QLength operator"" _tile(unsigned long long int x) {
+  return static_cast<double>(x) * tile;
+}
+constexpr QLength operator"" _court(unsigned long long int x) {
+  return static_cast<double>(x) * court;
+}
+
+} // namespace lib7842::units
