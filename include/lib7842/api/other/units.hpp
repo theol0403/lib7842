ae245068f6dd50a85aa12047c79a0459213c7dfb
Theo Lemay <theol0403@gmail.com>
2019-12-05 22:05:25 -0800

    Remove ips and ips2

--- include/lib7842/api/other/units.hpp
+++ include/lib7842/api/other/units.hpp
@@ -1,33 +1,10 @@
 #pragma once
-#include "okapi/api/units/QAcceleration.hpp"
-#include "okapi/api/units/QSpeed.hpp"
+#include "okapi/api/units/QLength.hpp"
 #include "okapi/api/units/RQuantity.hpp"
 
 namespace lib7842::units {
 
 using namespace okapi;
-
-/**
- * Inches per second
- */
-constexpr QSpeed ips = inch / second;
-constexpr QSpeed operator"" _ips(long double x) {
-  return static_cast<double>(x) * ips;
-}
-constexpr QSpeed operator"" _ips(unsigned long long int x) {
-  return static_cast<double>(x) * ips;
-}
-
-/**
- * Inches per second squared
- */
-constexpr QAcceleration ips2 = inch / (second * second);
-constexpr QAcceleration operator"" _ips2(long double x) {
-  return static_cast<double>(x) * ips2;
-}
-constexpr QAcceleration operator"" _ips2(unsigned long long int x) {
-  return static_cast<double>(x) * ips2;
-}
 
 /**
  * Units that represent a tile (2ft) and a court(12ft)
--- test/src/api/t_pathGenerator.cpp
+++ test/src/api/t_pathGenerator.cpp
@@ -1,25 +1,23 @@
 #include "test.hpp"
 
-using namespace lib7842::units;
-
 class PathGeneratorTest : public ::testing::Test, public PathGenerator {
 protected:
-  PursuitLimits limits {2_ips, 8_ips, 8_ips2, 1};
+  PursuitLimits limits {2_mps, 8_mps, 8_mps2, 0.03};
 };
 
 TEST_F(PathGeneratorTest, ComputeSingleCurvature) {
-  double straight = PathGenerator::calculateCurvature({0_in, 0_in}, {0_in, 5_in}, {0_in, 10_in});
+  double straight = PathGenerator::calculateCurvature({0_m, 0_m}, {0_m, 5_m}, {0_m, 10_m});
   ASSERT_EQ(straight, 0);
 
-  double curvature = PathGenerator::calculateCurvature({0_in, 0_in}, {3_in, 5_in}, {0_in, 10_in});
+  double curvature = PathGenerator::calculateCurvature({0_m, 0_m}, {3_m, 5_m}, {0_m, 10_m});
   ASSERT_NE(curvature, 0);
 
-  double turn = PathGenerator::calculateCurvature({0_in, 0_in}, {3_in, 5_in}, {0_in, 0_in});
+  double turn = PathGenerator::calculateCurvature({0_m, 0_m}, {3_m, 5_m}, {0_m, 0_m});
   ASSERT_EQ(turn, 0);
 }
 
 TEST_F(PathGeneratorTest, SetCurvatures) {
-  PursuitPath pathStraight({{0_in, 0_in}, {0_in, 5_in}, {0_in, 10_in}});
+  PursuitPath pathStraight({{0_m, 0_m}, {0_m, 5_m}, {0_m, 10_m}});
   PathGenerator::setCurvatures(pathStraight);
 
   ASSERT_EQ(pathStraight()[1]->getData<double>("curvature"), 0);
@@ -27,14 +25,14 @@ TEST_F(PathGeneratorTest, SetCurvatures) {
   ASSERT_EQ(pathStraight()[0]->getData<double>("curvature"), 0);
   ASSERT_EQ(pathStraight()[2]->getData<double>("curvature"), 0);
 
-  PursuitPath pathCurv({{0_in, 0_in}, {3_in, 5_in}, {0_in, 10_in}});
+  PursuitPath pathCurv({{0_m, 0_m}, {3_m, 5_m}, {0_m, 10_m}});
   PathGenerator::setCurvatures(pathCurv);
   ASSERT_NE(pathCurv()[1]->getData<double>("curvature"), 0);
 
   ASSERT_EQ(pathCurv()[0]->getData<double>("curvature"), 0);
   ASSERT_EQ(pathCurv()[2]->getData<double>("curvature"), 0);
 
-  PursuitPath pathTurn({{0_in, 0_in}, {3_in, 5_in}, {0_in, 0_in}});
+  PursuitPath pathTurn({{0_m, 0_m}, {3_m, 5_m}, {0_m, 0_m}});
   PathGenerator::setCurvatures(pathTurn);
   ASSERT_EQ(pathTurn()[1]->getData<double>("curvature"), 0);
 
@@ -43,22 +41,22 @@ TEST_F(PathGeneratorTest, SetCurvatures) {
 }
 
 TEST_F(PathGeneratorTest, SetMaxVelocity) {
-  PursuitPath path({{0_in, 0_in}, {0_in, 5_in}, {0_in, 10_in}});
+  PursuitPath path({{0_m, 0_m}, {0_m, 5_m}, {0_m, 10_m}});
   PathGenerator::setCurvatures(path);
   PathGenerator::setMaxVelocity(path, limits);
 
-  ASSERT_EQ(path()[0]->getData<QSpeed>("velocity").convert(ips), 8);
-  ASSERT_EQ(path()[1]->getData<QSpeed>("velocity").convert(ips), 8);
-  ASSERT_EQ(path()[2]->getData<QSpeed>("velocity").convert(ips), 0);
+  ASSERT_EQ(path()[0]->getData<QSpeed>("velocity").convert(mps), 8);
+  ASSERT_EQ(path()[1]->getData<QSpeed>("velocity").convert(mps), 8);
+  ASSERT_EQ(path()[2]->getData<QSpeed>("velocity").convert(mps), 0);
 }
 
 TEST_F(PathGeneratorTest, SetMaxVelocityTurn) {
-  PursuitPath path({{0_in, 0_in}, {3_in, 4_in}, {6_in, 10_in}, {5_in, 12_in}});
+  PursuitPath path({{0_m, 0_m}, {3_m, 4_m}, {6_m, 10_m}, {5_m, 12_m}});
   PathGenerator::setCurvatures(path);
   PathGenerator::setMaxVelocity(path, limits);
 
-  ASSERT_EQ(path()[0]->getData<QSpeed>("velocity").convert(ips), 8);
-  ASSERT_LT(path()[1]->getData<QSpeed>("velocity").convert(ips), 8);
-  ASSERT_LT(path()[2]->getData<QSpeed>("velocity").convert(ips), 8);
-  ASSERT_EQ(path()[3]->getData<QSpeed>("velocity").convert(ips), 0);
+  ASSERT_EQ(path()[0]->getData<QSpeed>("velocity").convert(mps), 8);
+  ASSERT_LT(path()[1]->getData<QSpeed>("velocity").convert(mps), 8);
+  ASSERT_LT(path()[2]->getData<QSpeed>("velocity").convert(mps), 8);
+  ASSERT_EQ(path()[3]->getData<QSpeed>("velocity").convert(mps), 0);
 }
