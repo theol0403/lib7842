73b1beb6b3709cd8f36c36c61d7143b114d446e7
Theo Lemay <theol0403@gmail.com>
2019-12-07 20:20:55 -0800

    Refactor PursuitLimits

--- include/lib7842/api/purePursuit/pursuitLimits.hpp
+++ include/lib7842/api/purePursuit/pursuitLimits.hpp
@@ -5,10 +5,24 @@
 namespace lib7842 {
 using namespace okapi;
 
-/**
- * Pure Pursuit limits.
- */
 struct PursuitLimits {
+  /**
+   * Pure Pursuit limits.
+   *
+   * @param iminVel The minimum velocity of the robot
+   * @param imaxVel The maximum velocity of the robot
+   * @param iaccel  The maximum acceleration of the robot
+   * @param ik      Used to control how much to slow down around turns. The velocity of the robot is
+   *                limited by this value divided by the path curvature. A higher curvature means a
+   *                lower velocity. This value is usually best around 1, 0.5 slows down at any
+   *                curvature in the path and 2 slows down around only a very sharp curvature.
+   */
+  PursuitLimits(const QSpeed& iminVel,
+                const QSpeed& imaxVel,
+                const QAcceleration& iaccel,
+                const QSpeed& ik) :
+    minVel(iminVel), maxVel(imaxVel), accel(iaccel), k(ik) {}
+
   /**
    * The minimum velocity of the robot
    */
@@ -25,11 +39,12 @@ struct PursuitLimits {
   QAcceleration accel {0_mps2};
 
   /**
-   * How much to slow down around turns. This value is usually best around 0.5-2, 0.5 tends to slow
-   * down around almost any curvature in the path, and 2 tends to slow down around only a very sharp
-   * curvature.
+   * Used to control how much to slow down around turns. The velocity of the robot is limited by
+   * this value divided by the path curvature. A higher curvature means a lower velocity. This value
+   * is usually best around 1, 0.5 slows down at any curvature in the path and 2 slows down around
+   * only a very sharp curvature.
    */
-  double k {0};
+  QSpeed k {0_mps};
 };
 
 } // namespace lib7842
