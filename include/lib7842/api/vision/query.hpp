be580f33cb147041857568e89dd83d1d41922278
Theo Lemay <theol0403@gmail.com>
2019-12-17 11:58:12 -0800

    Implement Query

---
+++ include/lib7842/api/vision/query.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <functional>
+
+namespace lib7842::Vision {
+
+class Object;
+
+class Query : public std::function<double(const Object& obj)> {
+public:
+  using function::function;
+  static Query sig, x, y, width, height, area, avgDim, centerX, centerY;
+
+  double getFrom(const Object& obj);
+};
+
+} // namespace lib7842::Vision
---
+++ src/lib7842/api/vision/query.cpp
@@ -0,0 +1,46 @@
+#include "query.hpp"
+#include "object.hpp"
+
+namespace lib7842::Vision {
+
+Query Query::sig = [](const Object& obj) {
+  return obj.sig;
+};
+
+Query Query::x = [](const Object& obj) {
+  return obj.x;
+};
+
+Query Query::y = [](const Object& obj) {
+  return obj.y;
+};
+
+Query Query::width = [](const Object& obj) {
+  return obj.width;
+};
+
+Query Query::height = [](const Object& obj) {
+  return obj.height;
+};
+
+Query Query::area = [](const Object& obj) {
+  return obj.width * obj.height;
+};
+
+Query Query::avgDim = [](const Object& obj) {
+  return (obj.width + obj.height) / 2.0;
+};
+
+Query Query::centerX = [](const Object& obj) {
+  return obj.x + obj.width / 2.0;
+};
+
+Query Query::centerY = [](const Object& obj) {
+  return obj.y + obj.height / 2.0;
+};
+
+double Query::getFrom(const Object& obj) {
+  return (*this)(obj);
+}
+
+} // namespace lib7842::Vision
