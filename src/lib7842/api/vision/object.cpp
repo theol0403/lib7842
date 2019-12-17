f43767c92c5d49cb49508fee098494389cc742cb
Theo Lemay <theol0403@gmail.com>
2019-12-17 12:28:23 -0800

    Implement Object

---
+++ src/lib7842/api/vision/object.cpp
@@ -0,0 +1,21 @@
+#include "object.hpp"
+
+namespace lib7842::Vision {
+
+Object::Object(const pros::vision_object& iobject) {
+  sig = iobject.signature;
+  x = iobject.left_coord;
+  y = iobject.top_coord;
+  width = iobject.width;
+  height = iobject.height;
+}
+
+void Object::set(const Query& iquery, double ivalue) {
+  iquery(*this) = ivalue;
+}
+
+double Object::get(const Query& iquery) const {
+  return iquery(*this);
+}
+
+} // namespace lib7842::Vision
