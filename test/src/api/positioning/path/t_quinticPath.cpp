b3c1a6509c5dac61ad61829e1ee1937d9d18d00f
Theo Lemay <theol0403@gmail.com>
2020-02-14 22:36:24 -0800

    Add quintic path test

--- test/src/api/positioning/path/t_quinticPath.cpp
+++ test/src/api/positioning/path/t_quinticPath.cpp
@@ -2,23 +2,80 @@
 
 TEST_CASE("QuinticPath test") {
 
-  GIVEN("a simple path") {
-    State start {0_in, 0_in, 0_deg};
-    QuinticPath path({start, {0_in, 1_in, 0_deg}}, 1);
+  SUBCASE("one point") {
+    THEN("one point should return nothing") {}
+    CHECK(QuinticPath({{0_in, 1_in, 90_deg}}, 1).generate(5)().size() == 0);
+  }
 
-    THEN("generating one step should return both points") {
-      auto ipath = path.generate(1);
-      CHECK(ipath().size() == 2);
-      CHECK(*ipath()[0] == start);
-      CHECK(ipath()[1]->y.convert(inch) == Approx(1));
+  SUBCASE("two points") {
+
+    GIVEN("a simple path") {
+      State start {0_in, 0_in, 0_deg};
+      QuinticPath path({start, {0_in, 1_in, 0_deg}}, 1);
+
+      THEN("generating one step should return both points") {
+        auto ipath = path.generate(1);
+        REQUIRE(ipath().size() == 2);
+        CHECK(*ipath()[0] == start);
+        CHECK(ipath()[1]->y.convert(inch) == Approx(1));
+      }
+
+      THEN("generating two steps should return interpolated points") {
+        auto ipath = path.generate(2);
+        REQUIRE(ipath().size() == 3);
+        CHECK(*ipath()[0] == start);
+        CHECK(ipath()[1]->y.convert(inch) == Approx(0.5));
+        CHECK(ipath()[2]->y.convert(inch) == Approx(1));
+      }
     }
 
-    THEN("generating two steps should return interpolated points") {
-      auto ipath = path.generate(2);
-      CHECK(ipath().size() == 3);
-      CHECK(*ipath()[0] == start);
-      CHECK(ipath()[1]->y.convert(inch) == Approx(0.5));
-      CHECK(ipath()[2]->y.convert(inch) == Approx(1));
+    GIVEN("an s curve") {
+      State start {0_in, 0_in, 90_deg};
+      QuinticPath path({start, {2_in, 4_in, 90_deg}}, 1);
+
+      THEN("generating one step should return both points") {
+        auto ipath = path.generate(1);
+        REQUIRE(ipath().size() == 2);
+        CHECK(*ipath()[0] == start);
+        CHECK(ipath()[1]->x.convert(inch) == Approx(2));
+        CHECK(ipath()[1]->y.convert(inch) == Approx(4));
+      }
+
+      THEN("generating two steps should return interpolated points") {
+        auto ipath = path.generate(2);
+        REQUIRE(ipath().size() == 3);
+        CHECK(*ipath()[0] == start);
+        CHECK(ipath()[1]->x.convert(inch) == Approx(1));
+        CHECK(ipath()[1]->y.convert(inch) == Approx(2));
+        CHECK(ipath()[2]->x.convert(inch) == Approx(2));
+        CHECK(ipath()[2]->y.convert(inch) == Approx(4));
+      }
+    }
+  }
+
+  SUBCASE("three points") {
+
+    GIVEN("a simple path") {
+      State start {0_in, 0_in, 0_deg};
+      QuinticPath path({start, {0_in, 1_in, 0_deg}, {0_in, 2_in, 0_deg}}, 1);
+
+      THEN("generating one step should return 3 points") {
+        auto ipath = path.generate(1);
+        REQUIRE(ipath().size() == 3);
+        CHECK(*ipath()[0] == start);
+        CHECK(ipath()[1]->y.convert(inch) == Approx(1));
+        CHECK(ipath()[2]->y.convert(inch) == Approx(2));
+      }
+
+      THEN("generating two steps should return 5 points") {
+        auto ipath = path.generate(2);
+        REQUIRE(ipath().size() == 5);
+        CHECK(*ipath()[0] == start);
+        CHECK(ipath()[1]->y.convert(inch) == Approx(0.5));
+        CHECK(ipath()[2]->y.convert(inch) == Approx(1));
+        CHECK(ipath()[3]->y.convert(inch) == Approx(1.5));
+        CHECK(ipath()[4]->y.convert(inch) == Approx(2));
+      }
     }
   }
 }
