47e8dbdfad0fe6a5ee3a3597831ff57b80289274
Theo Lemay <theol0403@gmail.com>
2020-02-15 21:52:25 -0800

    Revert "Remove smooth function"
    This reverts commit ac69882fe199dba0a7044a422977383ef5f7c6d7.

--- include/lib7842/api/positioning/path/discretePath.hpp
+++ include/lib7842/api/positioning/path/discretePath.hpp
@@ -112,6 +112,33 @@ public:
   }
 
   /**
+   * Smoothen the path
+   *
+   * @param iweight    The smooth weight
+   * @param itolerance The smooth tolerance
+   */
+  DiscretePath<T>& smoothen(double iweight, const QLength& itolerance) {
+    auto temp = copy();
+    double smoothWeight = 1.0 - iweight;
+    auto change = itolerance;
+    while (change >= itolerance) {
+      change = 0.0_m;
+      for (size_t i = 1; i < path.size() - 1; i++) {
+        for (size_t j = 0; j < 2; j++) {
+          auto aux = temp()[i]->at(j);
+          auto dataFac = iweight * (path[i]->at(j) - aux);
+          auto smoothFac =
+            smoothWeight * (temp()[i - 1]->at(j) + temp()[i + 1]->at(j) - (2.0 * aux));
+          temp()[i]->at(j) += (dataFac + smoothFac);
+          change = (aux - temp()[i]->at(j)).abs();
+        }
+      }
+    }
+    path = std::move(temp());
+    return *this;
+  }
+
+  /**
    * Interpolate the path using distance sampling.
    *
    * @param  iresolution The distance between each point
