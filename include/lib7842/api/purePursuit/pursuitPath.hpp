1e202c21fc3f4918e5b3d241fde4ec2c2427315a
Theo Lemay <theol0403@gmail.com>
2020-03-08 21:31:17 -0700

    Rework pursuitPath

--- include/lib7842/api/purePursuit/pursuitPath.hpp
+++ include/lib7842/api/purePursuit/pursuitPath.hpp
@@ -5,34 +5,49 @@
 
 namespace lib7842 {
 
+// forward declaration
+template <typename T> class PursuitPathHelper;
+using PursuitPath = PursuitPathHelper<DataPoint>;
+using PursuitStatePath = PursuitPathHelper<DataState>;
+
 /**
- * A DataPath that contains PursuitLimits.
+ * A Path that contains PursuitLimits.
  */
-class PursuitPath : public DataPath {
+template <typename T> class PursuitPathHelper : public DiscretePath<T> {
 public:
   /**
-   * A PursuitPath is treated like a DataPath
+   * A PursuitPath is treated like a Path
    */
-  using DataPath::DataPath;
+  using DiscretePath<T>::DiscretePath;
 
   /**
    * Set the limits.
    *
    * @param ilimits The pursuit limits
    */
-  void setLimits(const PursuitLimits& ilimits);
+  void setLimits(const PursuitLimits& ilimits) {
+    limits = ilimits;
+  }
 
   /**
    * Get the limits. Will throw an exception if no limits are set.
    *
    * @return The limits.
    */
-  PursuitLimits getLimits() const;
+  PursuitLimits getLimits() const {
+    if (limits) {
+      return limits.value();
+    } else {
+      throw std::runtime_error("PursuitPath::getLimits: no limits set");
+    }
+  }
 
   /**
    * @return true if limits have been set.
    */
-  bool hasLimits() const;
+  bool hasLimits() const {
+    return limits.has_value();
+  }
 
 protected:
   std::optional<PursuitLimits> limits {std::nullopt};
