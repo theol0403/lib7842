9a79712d66dc57c724e9aa744d5fc64566238517
Theo Lemay <theol0403@gmail.com>
2020-03-09 12:16:25 -0700

    Revert "Draft generateX"
    This reverts commit 73151c9ad4bb355e60487285d4202dd34a6fc995.

--- include/lib7842/api/purePursuit/pathGenerator.hpp
+++ include/lib7842/api/purePursuit/pathGenerator.hpp
@@ -16,15 +16,6 @@ public:
    */
   static PursuitPath generate(const SimplePath& ipath, const PursuitLimits& limits);
 
-  /**
-   * Generate a PursuitPath containing waypoint information for pure pursuit and heading information.
-   *
-   * @param  ipath  The path
-   * @param  limits The pure pursuit limits
-   * @return the generated path
-   */
-  static PursuitStatePath generateX(const StatePath& ipath, const PursuitLimits& limits);
-
 protected:
   /**
    * Sets the waypoint curvatures.
@@ -51,24 +42,5 @@ protected:
    * @return The curvature
    */
   static double calculateCurvature(const Vector& prev, const Vector& point, const Vector& next);
-
-  /**
-   * Sets the angular velocities of each point, using estimated time.
-   *
-   * @param ipath  The path
-   * @param limits The pure pursuit limits
-   */
-  static void setAngularVelocity(PursuitPath& ipath, const PursuitLimits& limits);
-
-  /**
-   * Estimates the time it will take to traverse between two points.
-   *
-   * @param  start  The start point
-   * @param  end    The end point
-   * @param  limits The pure pursuit limits
-   * @return The time.
-   */
-  static QTime calculateTime(const DataPoint& start, const DataPoint& end,
-                             const PursuitLimits& limits);
 };
 } // namespace lib7842
