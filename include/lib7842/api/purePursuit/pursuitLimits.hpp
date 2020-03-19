080cee7243d8c28eadd94a7277470a8dec2345bc
Theo Lemay <theol0403@gmail.com>
2020-03-18 19:59:21 -0700

    Add new pursuitLimits

--- include/lib7842/api/purePursuit/pursuitLimits.hpp
+++ include/lib7842/api/purePursuit/pursuitLimits.hpp
@@ -7,27 +7,12 @@ using namespace okapi;
 
 struct PursuitLimits {
   /**
-   * Pure Pursuit limits. Deceleration and final velocity is inferred from acceleration and minimum
-   * velocity.
-   *
-   * @param iminVel The minimum velocity of the robot throughout the entire path
-   * @param iaccel  The maximum acceleration of the robot
-   * @param imaxVel The maximum velocity of the robot
-   * @param ik      Used to control how much to slow down around turns. The velocity of the robot is
-   *                limited by this value divided by the path curvature. A higher curvature means a
-   *                lower velocity.
-   */
-  PursuitLimits(const QSpeed& iminVel, const QAcceleration& iaccel, const QSpeed& imaxVel,
-                const QSpeed& ik) :
-    minVel(iminVel), accel(iaccel), maxVel(imaxVel), decel(iaccel), finalVel(iminVel), k(ik) {}
-
-  /**
    * Pure Pursuit limits.
    *
    * @param iminVel   The minimum velocity of the robot throughout the entire path
-   * @param iaccel    The maximum acceleration of the robot
+   * @param iaccel    he acceleration of the robot from min velocity to max velocity
    * @param imaxVel   The maximum velocity of the robot
-   * @param idecel    The maximum deceleration of the robot
+   * @param idecel    The deceleration of the robot from max velocity to min velocity. Is positive
    * @param ifinalVel The final velocity of the robot at the end of the path. If lower than minimum
    *                  velocity, the robot will start decelerating sooner but will never reach this
    *                  velocity.
@@ -38,6 +23,64 @@ struct PursuitLimits {
   PursuitLimits(const QSpeed& iminVel, const QAcceleration& iaccel, const QSpeed& imaxVel,
                 const QAcceleration& idecel, const QSpeed& ifinalVel, const QSpeed& ik) :
     minVel(iminVel), accel(iaccel), maxVel(imaxVel), decel(idecel), finalVel(ifinalVel), k(ik) {}
+
+  /**
+   * Pure Pursuit limits. Deceleration and final velocity is inferred from acceleration and minimum
+   * velocity.
+   *
+   * @param iminVel The minimum velocity of the robot throughout the entire path
+   * @param iaccel  The acceleration of the robot from min velocity to max velocity
+   * @param imaxVel The maximum velocity of the robot
+   * @param ik      Used to control how much to slow down around turns. The velocity of the robot is
+   *                limited by this value divided by the path curvature. A higher curvature means a
+   *                lower velocity.
+   */
+  PursuitLimits(const QSpeed& iminVel, const QAcceleration& iaccel, const QSpeed& imaxVel,
+                const QSpeed& ik) :
+    minVel(iminVel), accel(iaccel), maxVel(imaxVel), decel(iaccel), finalVel(iminVel), k(ik) {}
+
+  /**
+   * Pure Pursuit limits. Time is used for acceleration and deceleration.
+   *
+   * @param iminVel   The minimum velocity of the robot throughout the entire path
+   * @param iaccel    The time for the robot to accelerate from the min velocity to max velocity
+   * @param imaxVel   The maximum velocity of the robot
+   * @param idecel    The time for the robot to decelerate from the max velocity to min velocity
+   * @param ifinalVel The final velocity of the robot at the end of the path. If lower than minimum
+   *                  velocity, the robot will start decelerating sooner but will never reach this
+   *                  velocity.
+   * @param ik        Used to control how much to slow down around turns. The velocity of the robot
+   *                  is limited by this value divided by the path curvature. A higher curvature
+   *                  means a lower velocity.
+   */
+  PursuitLimits(const QSpeed& iminVel, const QTime& iaccel, const QSpeed& imaxVel,
+                const QTime& idecel, const QSpeed& ifinalVel, const QSpeed& ik) :
+    minVel(iminVel),
+    accel((imaxVel - iminVel) / iaccel),
+    maxVel(imaxVel),
+    decel((imaxVel - iminVel) / idecel),
+    finalVel(ifinalVel),
+    k(ik) {}
+
+  /**
+   * Pure Pursuit limits. Deceleration and final velocity is inferred from acceleration and minimum
+   * velocity. Time is used for acceleration and deceleration.
+   *
+   * @param iminVel The minimum velocity of the robot throughout the entire path
+   * @param iaccel  The time for the robot to accelerate from the min velocity to max velocity
+   * @param imaxVel The maximum velocity of the robot
+   * @param ik      Used to control how much to slow down around turns. The velocity of the robot is
+   *                limited by this value divided by the path curvature. A higher curvature means a
+   *                lower velocity.
+   */
+  PursuitLimits(const QSpeed& iminVel, const QTime& iaccel, const QSpeed& imaxVel,
+                const QSpeed& ik) :
+    minVel(iminVel),
+    accel((imaxVel - iminVel) / iaccel),
+    maxVel(imaxVel),
+    decel(accel),
+    finalVel(iminVel),
+    k(ik) {}
 
   /**
    * The minimum velocity of the robot throughout the entire path
