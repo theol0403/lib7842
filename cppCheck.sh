f4efdbc600fc9d2958195c391510d498835af54a
Theo Lemay <theol0403@gmail.com>
2020-03-20 00:58:50 -0700

    Fix std version

--- cppCheck.sh
+++ cppCheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cppcheck --enable=all src test/src -Iinclude --suppress=unusedFunction --suppress=missingInclude --max-configs=1 --force --std=c++17 --inconclusive \
+cppcheck --enable=all src test/src -Iinclude --suppress=unusedFunction --suppress=missingInclude --max-configs=1 --force --inconclusive \
     --config-exclude=include/display --config-exclude=include/pros --suppress=*:*include/pros\* --suppress=*:*include/okapi\*
