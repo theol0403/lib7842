35c83e7add354785fa0aa714f1391690f41f0069
Theo Lemay <theol0403@gmail.com>
2020-03-20 00:28:03 -0700

    Simplify cppcheck

--- cppCheck.sh
+++ cppCheck.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-for I in $(find include/lib7842/ -type d); do
-    DIR=$DIR" -I$I"
-done
-
-for I in $(find include/okapi/ -type d); do
-    DIR=$DIR" -I$I"
-done
-
-for I in $(find include/pros/ -type d); do
-    DIR=$DIR" -I$I"
-done
-
-cppcheck --enable=all src test/src $DIR --suppress=unusedFunction --suppress=missingInclude
+cppcheck --enable=all src test/src -Iinclude --suppress=unusedFunction --suppress=missingInclude
