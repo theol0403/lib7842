5fc926efd0c2b60766f8c5b2f28b221b6a6a8611
Theo Lemay <theol0403@gmail.com>
2020-03-20 00:54:13 -0700

    Revert "Ask cppcheck for version"
    This reverts commit 68906c2377dfcde1acd8733603dc14b044ffd6ea.

--- cppCheck.sh
+++ cppCheck.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cppcheck --version
+cppcheck --enable=all src test/src -Iinclude --suppress=unusedFunction --suppress=missingInclude --max-configs=1 --force --std=c++17 --inconclusive \
+    --config-exclude=include/display --config-exclude=include/pros --suppress=*:*include/pros\* --suppress=*:*include/okapi\*
