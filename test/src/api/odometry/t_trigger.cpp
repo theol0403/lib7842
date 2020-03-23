b0e24843baa6486c6aadb7993c68fbfbfbb4c7a2
Theo Lemay <theol0403@gmail.com>
2020-03-22 22:48:09 -0700

    Trigger test

--- test/src/api/odometry/t_trigger.cpp
+++ test/src/api/odometry/t_trigger.cpp
@@ -3,37 +3,50 @@
 class MockOdomController : public OdomController {
 public:
   using OdomController::OdomController;
-  using OdomController::distanceErr;
+  using OdomController::_distanceErr;
 };
 
 TEST_CASE("Trigger test") {
-  auto model = std::make_shared<MockThreeEncoderXDriveModel>();
-  auto odom =
-    std::make_shared<CustomOdometry>(model, ChassisScales({{4_in, 10_in, 5_in, 4_in}, 360}));
-  auto chassis = std::make_shared<MockOdomController>(model, odom, nullptr, nullptr, nullptr, 0_in);
+  Trigger trigger;
 
-  auto trigger = chassis->trigger();
+  SUBCASE("logic tests") {
 
-  SUBCASE("nothing should return false") {
-    REQUIRE(!trigger());
-  }
+    SUBCASE("nothing should return false") {
+      REQUIRE(!trigger());
+    }
 
-  SUBCASE("requirements") {
-    SUBCASE("adding true") {
-      trigger.requirement([] { return true; });
-      REQUIRE(trigger());
-
-      SUBCASE("adding exception") {
-        trigger.exception([] { return true; });
-        REQUIRE(trigger());
-      }
-
+    SUBCASE("requirements") {
       SUBCASE("adding true") {
         trigger.requirement([] { return true; });
         REQUIRE(trigger());
 
-        SUBCASE("adding false") {
-          trigger.requirement([] { return false; });
+        SUBCASE("adding exception") {
+          trigger.exception([] { return true; });
+          REQUIRE(trigger());
+        }
+
+        SUBCASE("adding true") {
+          trigger.requirement([] { return true; });
+          REQUIRE(trigger());
+
+          SUBCASE("adding false") {
+            trigger.requirement([] { return false; });
+            REQUIRE(!trigger());
+
+            SUBCASE("adding exception") {
+              trigger.exception([] { return true; });
+              REQUIRE(trigger());
+            }
+          }
+        }
+      }
+
+      SUBCASE("adding false") {
+        trigger.requirement([] { return false; });
+        REQUIRE(!trigger());
+
+        SUBCASE("adding true") {
+          trigger.requirement([] { return true; });
           REQUIRE(!trigger());
 
           SUBCASE("adding exception") {
@@ -42,14 +55,9 @@ TEST_CASE("Trigger test") {
           }
         }
       }
-    }
 
-    SUBCASE("adding false") {
-      trigger.requirement([] { return false; });
-      REQUIRE(!trigger());
-
-      SUBCASE("adding true") {
-        trigger.requirement([] { return true; });
+      SUBCASE("adding exception") {
+        trigger.exception([] { return false; });
         REQUIRE(!trigger());
 
         SUBCASE("adding exception") {
@@ -58,42 +66,41 @@ TEST_CASE("Trigger test") {
         }
       }
     }
+  }
 
-    SUBCASE("adding exception") {
-      trigger.exception([] { return false; });
+  SUBCASE("chassis tests") {
+    auto model = std::make_shared<MockThreeEncoderXDriveModel>();
+    auto odom =
+      std::make_shared<CustomOdometry>(model, ChassisScales({{4_in, 10_in, 5_in, 4_in}, 360}));
+    auto chassis =
+      std::make_shared<MockOdomController>(model, odom, nullptr, nullptr, nullptr, 0_in);
+
+    SUBCASE("distanceErr") {
+      trigger.needs(chassis->distanceErr(5_mm));
+      chassis->_distanceErr = 10_mm;
       REQUIRE(!trigger());
+      chassis->_distanceErr = 1_mm;
+      REQUIRE(trigger());
+    }
 
-      SUBCASE("adding exception") {
-        trigger.exception([] { return true; });
-        REQUIRE(trigger());
-      }
+    SUBCASE("settledUtil") {
+      trigger.needs(
+        chassis->distanceSettledUtil(createTimeUtil(Supplier<std::unique_ptr<SettledUtil>>(
+          []() { return createSettledUtilPtr(5, 0, 20_ms); }))));
+      chassis->_distanceErr = 10_mm;
+      REQUIRE(!trigger());
+      pros::delay(30);
+      REQUIRE(!trigger());
+      chassis->_distanceErr = 1_mm;
+      REQUIRE(!trigger());
+      REQUIRE(!trigger()); // clear the derivative
+      pros::delay(30);
+      REQUIRE(trigger());
     }
   }
 
-  SUBCASE("distanceErr") {
-    trigger.distanceErr(5_mm);
-    chassis->distanceErr = 10_mm;
-    REQUIRE(!trigger());
-    chassis->distanceErr = 1_mm;
-    REQUIRE(trigger());
-  }
-
-  SUBCASE("settledUtil") {
-    trigger.distanceSettledUtil(createTimeUtil(
-      Supplier<std::unique_ptr<SettledUtil>>([]() { return createSettledUtilPtr(5, 0, 20_ms); })));
-    chassis->distanceErr = 10_mm;
-    REQUIRE(!trigger());
-    pros::delay(30);
-    REQUIRE(!trigger());
-    chassis->distanceErr = 1_mm;
-    REQUIRE(!trigger());
-    REQUIRE(!trigger()); // clear the derivative
-    pros::delay(30);
-    REQUIRE(trigger());
-  }
-
   SUBCASE("max time") {
-    trigger.maxTime(20_ms);
+    trigger.needs(Trigger::timePassed(20_ms));
     pros::delay(30);
     REQUIRE(!trigger());
     pros::delay(30);
