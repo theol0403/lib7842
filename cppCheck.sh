7d1e53455f4ae97dcdc2582db06209d19eaadd89
Theo Lemay <theol0403@gmail.com>
2020-03-27 14:27:15 -0700

    Speed up cppCheck

--- cppCheck.sh
+++ cppCheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cppcheck --enable=all src test/src -Iinclude --suppress=unusedFunction --suppress=missingInclude --max-configs=1 --force --std=c++17 --inconclusive \
-    --config-exclude=include/display --config-exclude=include/pros --suppress=*:*include/pros\* --suppress=*:*include/okapi\*
+    --config-exclude=test/include --config-exclude=include/display --config-exclude=include/pros --suppress=*:*include/pros\* --suppress=*:*include/okapi\* --suppress=*:*test/include\*
