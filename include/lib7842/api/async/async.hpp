5130b8db9bbbb6b18f2f00fa938cc091e6bbaaf0
Theo Lemay <theol0403@gmail.com>
2020-03-29 16:49:27 -0700

    Add comments to async

--- include/lib7842/api/async/async.hpp
+++ include/lib7842/api/async/async.hpp
@@ -5,19 +5,56 @@
 
 namespace lib7842 {
 
+/**
+ * A helper class for running actions asynchronously.
+ */
 class Async : public TaskWrapper {
 public:
   Async(const Async& itask) = delete;
   Async(Async&& itask) = default;
 
+  /**
+   * Run a function asynchronously.
+   *
+   * @param iaction The function to run.
+   */
   explicit Async(std::function<void()>&& iaction);
+
+  /**
+   * Run a function asynchronously after a trigger fires.
+   *
+   * @param itrigger The trigger to wait for before running the action.
+   * @param iaction  The function to run.
+   */
   explicit Async(Trigger&& itrigger, std::function<void()>&& iaction);
 
+  /**
+   * If a trigger was specified, force it to fire and run the action.
+   */
   virtual void forceStart();
+
+  /**
+   * Stop the internal task.
+   */
   virtual void forceStop();
 
+  /**
+   * Whether the action has been started.
+   *
+   * @return True if started, False otherwise.
+   */
   virtual bool hasStarted() const;
+
+  /**
+   * Whether the action has completed and returned.
+   *
+   * @return True if the action is complete, False otherwise.
+   */
   virtual bool isComplete() const;
+
+  /**
+   * Wait until the action is complete.
+   */
   virtual void waitUntilComplete() const;
 
 protected:
