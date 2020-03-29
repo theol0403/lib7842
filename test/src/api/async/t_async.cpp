072e5f4e8080c383ec2bb894e7cc308e5e2e6ee6
Theo Lemay <theol0403@gmail.com>
2020-03-29 15:58:27 -0700

    Improve test

--- test/src/api/async/t_async.cpp
+++ test/src/api/async/t_async.cpp
@@ -6,7 +6,23 @@ TEST_CASE("Async") {
     bool ran = false;
     Async async([&]() { ran = true; });
     REQUIRE(!async.hasStarted());
+    REQUIRE(!ran);
     async.waitUntilComplete();
     REQUIRE(async.isComplete());
+    REQUIRE(ran);
+  }
+
+  SUBCASE("trigger functionality") {
+    bool ran = false;
+    Async async(Trigger().requirement([]() { return false; }), [&]() { ran = true; });
+    REQUIRE(!async.hasStarted());
+    REQUIRE(!ran);
+    pros::delay(1);
+    REQUIRE(!async.hasStarted());
+    REQUIRE(!ran);
+    async.forceStart();
+    async.waitUntilComplete();
+    REQUIRE(async.isComplete());
+    REQUIRE(ran);
   }
 }
