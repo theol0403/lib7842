e487331c29b8121e9f8e9fe96c85ada0124da965
Theo Lemay <theol0403@gmail.com>
2020-03-31 15:14:21 -0700

    Add wrap

--- src/lib7842/api/purePursuit/unicycleFollower.cpp
+++ src/lib7842/api/purePursuit/unicycleFollower.cpp
@@ -15,52 +15,54 @@ double sinc(double x) {
   return x == 0 ? 1 : sin(x) / x;
 }
 
+double wrap180(double a) {
+  return atan2(std::sin(a), std::cos(a));
+}
+
 void UnicycleFollower::seek(const State& iref, double ih, double ikv, double ika) {
   auto rate = global::getTimeUtil()->getRate();
 
-  bool isFinished = false; // loop until the robot is considered to have finished the path
-  while (!isFinished) {
-    State pos = State(odometry->getState(StateMode::CARTESIAN));
+  State pos = State(odometry->getState(StateMode::CARTESIAN));
 
-    double x_ref = iref.y.convert(meter);
-    double y_ref = iref.x.convert(meter);
-    double theta_ref = (90_deg - iref.theta).convert(radian);
+  double x_ref = iref.y.convert(meter);
+  double y_ref = iref.x.convert(meter);
+  double theta_ref = (90_deg - iref.theta).convert(radian);
 
-    double x = pos.y.convert(meter);
-    double y = pos.x.convert(meter);
-    double theta = (90_deg - pos.theta).convert(radian);
+  double x = pos.y.convert(meter);
+  double y = pos.x.convert(meter);
+  double theta = (90_deg - pos.theta).convert(radian);
 
-    double ex = cos(theta) * (x_ref - x) - sin(theta) * (y_ref - y);
-    double ey = sin(theta) * (x_ref - x) + cos(theta) * (y_ref - y);
+  double ex = cos(theta) * (x_ref - x) - sin(theta) * (y_ref - y);
+  double ey = sin(theta) * (x_ref - x) + cos(theta) * (y_ref - y);
 
-    double p = sqrt(ex * ex + ey * ey);
+  double p = sqrt(ex * ex + ey * ey);
 
-    double phi = atan2(ey, ex) - theta_ref;
-    double a = atan2(ey, ex) - theta;
+  double phi = atan2(ey, ex) - theta_ref;
+  phi = wrap180(phi);
+  double a = atan2(ey, ex) - theta;
+  a = wrap180(a);
 
-    double v = ikv * p * cos(a);
-    double w = ika * a + ikv * sinc(2 * a) * (a + ih * phi);
+  double v = ikv * p * cos(a);
+  double w = ika * a + ikv * sinc(2 * a) * (a + ih * phi);
 
-    auto vel = v * mps;
-    auto turnVel = w * radps;
+  auto vel = v * mps;
+  auto turnVel = w * radps;
 
-    QAngularSpeed wheelVel = (vel / (1_pi * chassisScales.wheelDiameter)) * 360_deg;
-    QAngularSpeed wheelTurnVel = turnVel * chassisScales.wheelTrack / chassisScales.wheelDiameter;
+  QAngularSpeed wheelVel = (vel / (1_pi * chassisScales.wheelDiameter)) * 360_deg;
+  QAngularSpeed wheelTurnVel = turnVel * chassisScales.wheelTrack / chassisScales.wheelDiameter;
 
-    double wheelPower = (wheelVel / gearset).convert(number);
-    double turnPower = (wheelTurnVel / gearset).convert(number);
+  double wheelPower = (wheelVel / gearset).convert(number);
+  double turnPower = (wheelTurnVel / gearset).convert(number);
 
-    auto finalPower = clamp(wheelPower, turnPower);
+  auto finalPower = clamp(wheelPower, turnPower);
 
-    double left = finalPower[0] + finalPower[1];
-    double right = finalPower[0] - finalPower[1];
+  double left = finalPower[0] + finalPower[1];
+  double right = finalPower[0] - finalPower[1];
 
-    model->tank(left, right);
-    rate->delayUntil(10_ms);
-  }
+  model->tank(left, right);
 }
 
-std::valarray<double> clamp(double v, double w) {
+std::valarray<double> UnicycleFollower::clamp(double v, double w) {
   auto sigma = std::max({v, w, 1.0});
 
   double v_c;
