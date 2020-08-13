e014681ff618242d1e2f37b280fc834cf07b878a
Theo Lemay <theol0403@gmail.com>
2020-08-12 18:36:05 -0700

    Clean

--- src/lib7842/test/test.cpp
+++ src/lib7842/test/test.cpp
@@ -1,11 +1,12 @@
 #include "lib7842/api/other/global.hpp"
-#define DOCTEST_CONFIG_NO_WINDOWS_SEH
-#define DOCTEST_CONFIG_NO_POSIX_SIGNALS
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "lib7842/test/mocks.hpp"
 
 namespace test {
 int runUnitTests(int argc, char** argv) {
+  global::setLogger(
+    std::make_shared<Logger>(std::make_unique<MockTimer>(), "/ser/sout", Logger::LogLevel::off));
+
   doctest::Context context;
   context.applyCommandLine(argc, argv);
   return context.run(); // run
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -7,12 +7,6 @@ using namespace test;
 int lvglMain();
 
 int main(int argc, char** argv) {
-  Logger::setDefaultLogger(
-    std::make_shared<Logger>(std::make_unique<MockTimer>(), "/dev/stdout", Logger::LogLevel::warn));
-
-  global::setLogger(
-    std::make_shared<Logger>(std::make_unique<MockTimer>(), "/dev/stdout", Logger::LogLevel::off));
-
   global::setTimeUtil(std::make_shared<TimeUtil>(createTimeUtil()));
 
   if (argc > 1 && std::string(argv[1]) == "lvgl") {
@@ -26,11 +20,5 @@ int main(int argc, char** argv) {
 extern "C" {
 namespace pros::c {
 void delay(const uint32_t milliseconds) { usleep(1000 * milliseconds); }
-
-void task_delay_until(uint32_t* const prev_time, const uint32_t delta) {
-  (void)prev_time;
-  usleep(1000 * delta);
-}
-
 } // namespace pros::c
 } // extern "C"
