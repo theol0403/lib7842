72664df6db5175c0e01e431f268a7b59d43fa7ac
Theo Lemay <theol0403@gmail.com>
2020-08-17 10:50:19 -0700

    Piecewise tests

--- src/lib7842/api/positioning/path/piecewise.cpp
+++ src/lib7842/api/positioning/path/piecewise.cpp
@@ -3,7 +3,34 @@
 #include "line.hpp"
 namespace test {
 TEST_CASE("Piecewise") {
-  auto p = make_piecewise<Line>({{{0_m, 0_m}, {1_m, 1_m}}, {{0_m, 0_m}, {1_m, 1_m}}})
-             .step(StepBy::T(0.01));
+  SUBCASE("Explicit") {
+    auto l = Piecewise<Line, 2>({{{0_m, 0_m}, {1_m, 1_m}}, {{1_m, 1_m}, {2_m, 2_m}}})
+               .step(StepBy::Count(5))
+               .generate();
+    REQUIRE(l.size() == 6);
+    for (size_t i = 0; i < l.size(); ++i) {
+      REQUIRE(l[i] == State(i / 2.5 * meter, i / 2.5 * meter, 45_deg));
+    }
+  }
+
+  SUBCASE("Deduction") {
+    auto l = Piecewise({Line {{0_m, 0_m}, {1_m, 1_m}}, Line {{1_m, 1_m}, {2_m, 2_m}}})
+               .step(StepBy::Count(5))
+               .generate();
+    REQUIRE(l.size() == 6);
+    for (size_t i = 0; i < l.size(); ++i) {
+      REQUIRE(l[i] == State(i / 2.5 * meter, i / 2.5 * meter, 45_deg));
+    }
+  }
+
+  SUBCASE("Helper") {
+    auto l = make_piecewise<Line>({{{0_m, 0_m}, {1_m, 1_m}}, {{1_m, 1_m}, {2_m, 2_m}}})
+               .step(StepBy::Count(5))
+               .generate();
+    REQUIRE(l.size() == 6);
+    for (size_t i = 0; i < l.size(); ++i) {
+      REQUIRE(l[i] == State(i / 2.5 * meter, i / 2.5 * meter, 45_deg));
+    }
+  }
 }
 } // namespace test
