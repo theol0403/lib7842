63a9a823c2ddc94f006ad57da520ded80c76c498
Theo Lemay <theol0403@gmail.com>
2020-08-18 01:21:07 -0700

    Comment path

--- include/lib7842/api/positioning/path/path.hpp
+++ include/lib7842/api/positioning/path/path.hpp
@@ -6,16 +6,46 @@
 
 namespace lib7842 {
 
+/**
+ * Base path class. Main method of sampling is via calc, according to a t parameter that is in the
+ * range of [0, 1].
+ */
 class Path {
 public:
   constexpr virtual ~Path() = default;
 
+  /**
+   * Sample the path at t.
+   *
+   * @param  t Where along the path to sample, in the range of [0, 1]
+   * @return the sampled point at t
+   */
   constexpr virtual State calc(double t) const = 0;
 
+  /**
+   * Sample the curvature of the path at t. Curvature is the inverse of the radius.
+   *
+   * @param  t Where along the path to sample, in the range of [0, 1]
+   * @return the curvature at t
+   */
   constexpr virtual QCurvature curvature(double t) const = 0;
 
+  /**
+   * Sample the velocity of the path at t. Velocity is the ratio between distance traveled and
+   * change in t.
+   *
+   * @param t Where along the path to sample, in the range of [0, 1]
+   * @return the velocity at t
+   */
   constexpr virtual QLength velocity(double /*t*/) const { return length(); }
 
+  /**
+   * Calculate the length of the path. This method has a default implementation that tries to fit
+   * lines onto the path and sums their length.
+   *
+   * @param  resolution The number of lines to fit to the path.
+   * @return the length of the path
+   */
   constexpr virtual QLength length(double resolution = 100) const {
     QLength len {0.0};
     for (size_t i = 0; i < resolution; i++) {
@@ -24,18 +54,32 @@ public:
     return len;
   }
 
+  /**
+   * Using the velocity of the path, calculate how much to increment t to travel a certain distance.
+   *
+   * @param  t    The previous value of t.
+   * @param  dist The desired distance to travel.
+   * @return the best estimate of the new value of t
+   */
   constexpr virtual double t_at_dist_travelled(double t, const QLength& dist) const {
     return t + (dist / velocity(t).abs()).convert(number);
   }
 };
 
 template <typename CRTP> struct RuntimePath {
+  /**
+   * Return a Stepper that contains a reference to the path and a given StepBy.
+   */
   template <typename S> requires(!ConstStepper<S>) constexpr auto step(S&& s) const& {
     return Stepper(static_cast<const CRTP&>(*this), std::forward<S>(s));
   }
   template <typename S> requires(!ConstStepper<S>) constexpr auto step(S&& s) && {
     return Stepper(static_cast<CRTP&&>(*this), std::forward<S>(s));
   }
+  /**
+   * Generate the path given a StepBy. Generate means to sample the whole path and return an array
+   * of points.
+   */
   template <typename S> requires(!ConstStepper<S>) auto generate(S&& s) const& {
     return Stepper(static_cast<const CRTP&>(*this), std::forward<S>(s)).generate();
   }
@@ -45,12 +89,19 @@ template <typename CRTP> struct RuntimePath {
 };
 template <typename CRTP> struct ConstPath {
   consteval ConstPath() = default;
+    /**
+     * Return a Stepper that contains a reference to the path and a given StepBy.
+     */
   template <typename S> requires ConstStepper<S> consteval auto step(S&& s) const& {
     return Stepper(static_cast<const CRTP&>(*this), std::forward<S>(s));
   }
   template <typename S> requires ConstStepper<S> consteval auto step(S&& s) && {
     return Stepper(static_cast<CRTP&&>(*this), std::forward<S>(s));
   }
+  /**
+   * Generate the path given a StepBy. Generate means to sample the whole path and return an array
+   * of points.
+   */
   template <typename S> requires ConstStepper<S> consteval auto generate(S&& s) const& {
     return Stepper(static_cast<const CRTP&>(*this), std::forward<S>(s)).generate();
   }
@@ -67,3 +118,4 @@ struct PathHelper : public RuntimePath<CRTP>, public ConstPath<CRTP>, public Pat
   using ConstPath<CRTP>::generate;
 };
 } // namespace lib7842
+
