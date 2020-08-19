3ac3ede2a0f5f9663bdafa769cbf701d2a9e619c
Theo Lemay <theol0403@gmail.com>
2020-08-18 23:28:57 -0700

    Comment line

--- include/lib7842/api/positioning/path/line.hpp
+++ include/lib7842/api/positioning/path/line.hpp
@@ -6,16 +6,38 @@
 
 namespace lib7842 {
 
+/**
+ * This class describes a line which connects two points together.
+ */
 class Line : public PathHelper<Line> {
 public:
+  /**
+   * Create a new line given the coordinates of two points.
+   *
+   * @param istart The start point
+   * @param iend   The end point
+   */
   constexpr Line(const Vector& istart, const Vector& iend) :
     start(istart, istart.angleTo(iend)), end(iend, start.theta) {}
   constexpr ~Line() override = default;
 
+  /**
+   * Sample the point along the path which is between the start and end points using linear
+   * interpolation given t.
+   *
+   * @param  t The ratio of interpolation between the two points in the range of [0, 1].
+   * @return The interpolated point.
+   */
   constexpr State calc(double t) const override { return start + (end - start) * t; }
 
+  /**
+   * The curvature of a line is always zero.
+   */
   constexpr QCurvature curvature(double /*t*/) const override { return 0 / meter; }
 
+  /**
+   * Calculate the length of the line which is the distance between the start and end points.
+   */
   constexpr QLength length(double /*resolution*/ = 0) const override { return start.distTo(end); }
 
 protected:
