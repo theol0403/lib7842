f0126cd07bcfbf8e12b4c412000e3a232ff39d3c
Theo Lemay <theol0403@gmail.com>
2020-08-28 13:18:25 -0700

    Size_t

--- include/lib7842/api/positioning/spline/stepper.hpp
+++ include/lib7842/api/positioning/spline/stepper.hpp
@@ -56,9 +56,9 @@ namespace StepBy {
 
 class Count {
   template <class P>
-  class iterator : public std::iterator<const std::forward_iterator_tag, State, int> {
+  class iterator : public std::iterator<const std::forward_iterator_tag, State, size_t> {
   public:
-    constexpr iterator(const P& ip, int ic, int ii) : p(ip), c(ic), i(ii) {}
+    constexpr iterator(const P& ip, size_t ic, size_t ii) : p(ip), c(ic), i(ii) {}
     constexpr bool operator!=(const iterator& rhs) const { return i != (rhs.i + 1); }
     constexpr State operator*() const { return p.calc(static_cast<double>(i) / c); }
     constexpr State operator->() const { return *(*this); }
@@ -69,22 +69,22 @@ class Count {
 
   protected:
     const P& p;
-    const int c;
-    int i;
+    const size_t c;
+    size_t i;
   };
 
 public:
-  consteval explicit Count(int ic) : c(ic) {
+  consteval explicit Count(size_t ic) : c(ic) {
     ic > 0 ? true : throw std::invalid_argument("StepBy::Count: count must be greater than zero");
   }
   template <class P> constexpr auto begin(const P& ip) const { return iterator<P>(ip, c, 0); }
   template <class P> constexpr auto end(const P& ip) const { return iterator<P>(ip, c, c); }
-  const int c;
+  const size_t c;
 };
 
 consteval Count T(double t) {
   return Count(t > 0.0 && t <= 1.0
-                 ? static_cast<int>(1.0 / t)
+                 ? static_cast<size_t>(1.0 / t)
                  : throw std::invalid_argument(
                      "StepBy::T: t must be greater than zero and less than or equal to 1"));
 }
@@ -118,10 +118,11 @@ public:
 };
 
 template <size_t C> class ConstCount {
+  static_assert(C > 0, "StepBy::ConstCount: C must be greater than zero");
   template <class P>
-  class iterator : public std::iterator<const std::forward_iterator_tag, State, int> {
+  class iterator : public std::iterator<const std::forward_iterator_tag, State, size_t> {
   public:
-    consteval iterator(const P& ip, int ii) : p(ip), i(ii) {}
+    consteval iterator(const P& ip, size_t ii) : p(ip), i(ii) {}
     consteval bool operator!=(const iterator& rhs) const { return i != (rhs.i + 1); }
     consteval State operator*() const { return p.calc(static_cast<double>(i) / C); }
     consteval State operator->() const { return *(*this); }
@@ -132,7 +133,7 @@ template <size_t C> class ConstCount {
 
   protected:
     const P& p;
-    int i;
+    size_t i;
   };
 
 public:
