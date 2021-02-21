32ebd4867fd26a199b3e4d543e35cf39c5fbdf79
Theo Lemay <theol0403@gmail.com>
2021-02-20 21:01:16 -0800

    Extract motion

--- include/lib7842/api/trajectory/generator.hpp
+++ include/lib7842/api/trajectory/generator.hpp
@@ -19,8 +19,16 @@ public:
     {}
   }
 
-  void follow(const Spline& spline, bool forward = true, const Number& istart_v = 0_pct,
-              const Number& iend_v = 0_pct, const Number& itop_v = 100_pct) const {
+  struct Step {
+    State p;
+    KinematicState k;
+    QAngularSpeed w;
+  };
+
+  using Runner = std::function<void(const Step&)>;
+
+  void generate(const Spline& spline, const Runner& runner, const Number& istart_v = 0_pct,
+                const Number& iend_v = 0_pct, const Number& itop_v = 100_pct) const {
     QLength length = spline.length();
     Trapezoidal profile(limits, length, istart_v, iend_v, itop_v);
 
@@ -29,14 +37,14 @@ public:
     QLength dist = 0_m;
     State pos = spline.calc(t);
     QAngle theta = pos.theta;
-    QSpeed vel = profile.calc(0_s).v;
-    if (vel == 0_mps) { vel = profile.calc(dt).v; }
+    KinematicState k = profile.begin();
+    if (k.v == 0_mps) { k = profile.calc(dt); }
 
     Rate rate;
     while (dist <= length && t <= 1) {
       // limit velocity according to approximation of the curvature during the next timeslice
       QCurvature curvature = spline.curvature(t);
-      QSpeed vel_max = std::min(vel, limits.max_vel_at_curvature(curvature));
+      QSpeed vel_max = std::min(k.v, limits.max_vel_at_curvature(curvature));
       // project where along the spline we will be after dt, given approximate velocity
       double t_n = spline.t_at_dist_travelled(t, vel_max * dt);
       State pos_new = spline.calc(t_n);
@@ -46,43 +54,51 @@ public:
       // update internal theta representation
       theta += angular_vel * dt;
       // limit profiled velocity to angular velocity
-      vel = std::min(vel, limits.max_vel_at_w(angular_vel));
+      k.v = std::min(k.v, limits.max_vel_at_w(angular_vel));
 
       // calculate distance traveled
-      QLength d_dist = vel * dt;
+      QLength d_dist = k.v * dt;
       dist += d_dist;
       // calculate where along the spline we will be at the end of the timeslice
       t = spline.t_at_dist_travelled(t, d_dist);
 
       // run trajectory
-      moveStep(vel, angular_vel, forward);
-      rate.delayUntil(dt);
+      runner({pos, k, angular_vel});
+      // moveStep(vel, angular_vel);
+      // rate.delayUntil(dt);
 
       // update new position
       pos = spline.calc(t);
       // calculate new velocity
-      vel = profile.calc(dist).v;
+      k = profile.calc(dist);
     }
-    moveStep(profile.end().v, 0_rpm, forward);
+    KinematicState end = profile.end();
+    if (end.v == 0_mps) { runner({pos, end, 0_rpm}); }
   }
 
-  void moveStep(const QSpeed& v, const QAngularSpeed& w, bool forward) const {
-    QSpeed left = v - (w / radian * scales.wheelTrack) / 2;
-    QSpeed right = v + (w / radian * scales.wheelTrack) / 2;
+  void follow(const Spline& spline, bool forward = true, const Number& istart_v = 0_pct,
+              const Number& iend_v = 0_pct, const Number& itop_v = 100_pct) {
+    generate(
+      spline,
+      [&](const Step& s) {
+        QSpeed left = s.k.v - (s.w / radian * scales.wheelTrack) / 2;
+        QSpeed right = s.k.v + (s.w / radian * scales.wheelTrack) / 2;
 
-    QAngularSpeed leftWheel = (left / (1_pi * scales.wheelDiameter)) * 360_deg;
-    QAngularSpeed rightWheel = (right / (1_pi * scales.wheelDiameter)) * 360_deg;
+        QAngularSpeed leftWheel = (left / (1_pi * scales.wheelDiameter)) * 360_deg;
+        QAngularSpeed rightWheel = (right / (1_pi * scales.wheelDiameter)) * 360_deg;
 
-    auto leftSpeed = (leftWheel / gearset).convert(number);
-    auto rightSpeed = (rightWheel / gearset).convert(number);
+        auto leftSpeed = (leftWheel / gearset).convert(number);
+        auto rightSpeed = (rightWheel / gearset).convert(number);
 
-    if (forward) {
-      model->tank(leftSpeed, rightSpeed);
-    } else {
-      model->tank(-rightSpeed, -leftSpeed);
-    }
-    // model.left(leftSpeed);
-    // model.right(rightSpeed);
+        if (forward) {
+          model->tank(leftSpeed, rightSpeed);
+        } else {
+          model->tank(-rightSpeed, -leftSpeed);
+        }
+        // model.left(leftSpeed);
+        // model.right(rightSpeed);
+      },
+      istart_v, iend_v, itop_v);
   }
 
 protected:
